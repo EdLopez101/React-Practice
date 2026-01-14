fe70eaddc44638df890eb7e5e3bf80bc4e5e5d03
Ed <elngutaijfgc@gmail.com>
2026-01-14 17:00:08 -0500

    Added reusable button component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <Button />
+      <Button classes="btn btn-primary">Primary</Button>
+      <Button classes="btn btn-danger">Danger</Button>
+      <Button classes="btn btn-link">Link</Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,16 @@
-const Button = () => {
-  return <div>Button</div>;
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  classes: string;
+}
+
+const Button = ({ children, classes }: Props) => {
+  return (
+    <button type="button" className={classes}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
